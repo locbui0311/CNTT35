e16834b6105c73282a1531a2ac1479218f77af18
LocBui <LocBui@DESKTOP-IJO1K2O>
2022-11-23 09:41:32 +0700

    Add project files.

---
+++ CNTT35/Content/js/site.js
@@ -0,0 +1,56 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*  ---------------------------------------------------
+    Template Name: Ogani
+    Description:  Ogani eCommerce  HTML Template
+    Author: Colorlib
+    Author URI: https://colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+jQuery(document).ready(($) => {
+    $('.quantity').on('click', '.plus', function (e) {
+        let $input = $(this).prev('input.qty');
+        let val = parseInt($input.val());
+        $input.val(val + 1).change();
+    });
+
+    $('.quantity').on('click', '.minus',
+        function (e) {
+            let $input = $(this).next('input.qty');
+            var val = parseInt($input.val());
+            if (val > 0) {
+                $input.val(val - 1).change();
+            }
+        });
+});
+
+function getVals() {
+    // Get slider values
+    let parent = this.parentNode;
+    let slides = parent.getElementsByTagName("input");
+    let slide1 = parseFloat(slides[0].value);
+    let slide2 = parseFloat(slides[1].value);
+    // Neither slider will clip the other, so make sure we determine which is larger
+    if (slide1 > slide2) { let tmp = slide2; slide2 = slide1; slide1 = tmp; }
+
+    let displayElement = parent.getElementsByClassName("rangeValues")[0];
+    displayElement.innerHTML = + slide1 + "đ" + " - " + slide2 + "đ ";
+}
+
+window.onload = function () {
+    // Initialize Sliders
+    let sliderSections = document.getElementsByClassName("range-slider");
+    for (let x = 0; x < sliderSections.length; x++) {
+        let sliders = sliderSections[x].getElementsByTagName("input");
+        for (let y = 0; y < sliders.length; y++) {
+            if (sliders[y].type === "range") {
+                sliders[y].oninput = getVals;
+                // Manually trigger event first time to display values
+                sliders[y].oninput();
+            }
+        }
+    }
+}
